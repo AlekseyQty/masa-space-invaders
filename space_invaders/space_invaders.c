d9bbba9a7960d49b172cabe1208ddf51fd5d1b74
Aleksei <saratkin.aleksey@gmail.com>
2022-09-26 19:52:55 +0300

    Moving rectangle with controls from keyboard done

--- space_invaders/space_invaders.c
+++ space_invaders/space_invaders.c
@@ -3,58 +3,163 @@
 
 #include "framework.h"
 #include "space_invaders.h"
+#include "stdio.h"
+#include <time.h>
 
 #define MAX_LOADSTRING 100
+
+// Custom definitions
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
+#define PLAYER_SPEED 4
 
 // Global Variables:
 HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 
+
+// My declarations
+RECT player;
+RECT rct;
+BOOL isGameRunning = TRUE;
+
+// My functionds
+
+void initPlayer(HDC dc) {
+	srand(time(NULL));
+
+	player.left = rand() % WINDOW_WIDTH;
+	player.top = rand() % WINDOW_HEIGHT;
+	player.right = player.left + 100;
+	player.bottom = player.top + 100;
+
+	SelectObject(dc, CreateSolidBrush(RGB(250, 150, 150)));
+	Rectangle(dc, player.left, player.top, player.right, player.bottom);
+}
+
+void movePlayerRight() {
+	player.right += PLAYER_SPEED;
+	if (player.right >= WINDOW_WIDTH) {
+		player.right = 100;
+	}
+	player.left = player.right - 100;
+}
+
+void movePlayerLeft() {
+	player.left -= PLAYER_SPEED;
+	if (player.left <= 0) {
+		player.left = WINDOW_WIDTH-100;
+	}
+	player.right = player.left + 100;
+	//player.bottom = player.top + 100;
+}
+
+void movePlayerDown() {
+	player.top += PLAYER_SPEED;
+	if (player.top > WINDOW_HEIGHT) {
+		player.top = -100;
+	}
+	player.bottom = player.top + 100;
+}
+
+void movePlayerUp() {
+	player.top -= PLAYER_SPEED;
+	if (player.top < -100) {
+		player.top = WINDOW_HEIGHT;
+	}
+	player.bottom = player.top + 100;
+}
+
+
+
+void updateWindow(HDC dc) {
+	HDC memDC = CreateCompatibleDC(dc);
+	HBITMAP memBM = CreateCompatibleBitmap(dc, rct.right - rct.left, rct.bottom - rct.top);
+	SelectObject(memDC, memBM);
+
+	SelectObject(memDC, CreateSolidBrush(RGB(255, 255, 255)));
+	Rectangle(memDC, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+	SelectObject(memDC, CreateSolidBrush(RGB(250, 150, 150)));
+	Rectangle(memDC, player.left, player.top, player.right, player.bottom);
+
+	// Copy from Memory Device Context to Device Context
+	BitBlt(dc, 0, 0, rct.right - rct.left, rct.bottom - rct.top, memDC, 0, 0, SRCCOPY);
+	DeleteDC(memDC);
+	DeleteObject(memBM);
+}
+
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
+HWND                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: Place code here.
+	// TODO: Place code here.
 
-    // Initialize global strings
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_SPACEINVADERS, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+	// Initialize global strings
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_SPACEINVADERS, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
 
-    // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+	HWND hWnd = InitInstance(hInstance, nCmdShow);
+	// Perform application initialization:
+	if (!hWnd)
+	{
+		return FALSE;
+	}
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_SPACEINVADERS));
+	HDC dc = GetDC(hWnd);
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_SPACEINVADERS));
+	MSG msg;
 
-    MSG msg;
 
-    // Main message loop:
-    while (GetMessage(&msg, NULL, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
+	// Main message loop:
+	OutputDebugString(L"GAME INIT\n");
+	initPlayer(dc);
 
-    return (int) msg.wParam;
+	while (isGameRunning)
+	{
+		if (PeekMessageA(&msg, hWnd, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		else
+		{
+			if (GetKeyState(VK_ESCAPE) < 0) {
+				isGameRunning = FALSE;
+			}
+			if (GetAsyncKeyState(VK_RIGHT) < 0) {
+				movePlayerRight();
+			}
+			if (GetAsyncKeyState(VK_LEFT) < 0) {
+				movePlayerLeft();
+			}
+			if (GetAsyncKeyState(VK_UP) < 0) {
+				movePlayerUp();
+			}
+			if (GetAsyncKeyState(VK_DOWN) < 0) {
+				movePlayerDown();
+			}
+			updateWindow(dc);
+			Sleep(5);
+		}
+
+	}
+	return (int)msg.wParam;
 }
-
 
 
 //
@@ -64,23 +169,25 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
+	// setup the class instance of our Win32 application
+	WNDCLASSEXW wcex;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SPACEINVADERS));
-    wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_SPACEINVADERS);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SPACEINVADERS));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_SPACEINVADERS);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-    return RegisterClassExW(&wcex);
+	// attempt to register the win32 application class
+	return RegisterClassExW(&wcex);
 }
 
 //
@@ -93,22 +200,22 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //        In this function, we save the instance handle in a global variable and
 //        create and display the main program window.
 //
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+HWND InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Store instance handle in our global variable
+	hInst = hInstance; // Store instance handle in our global variable
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		10, 10, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL, hInstance, NULL);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+	if (!hWnd)
+	{
+		return FALSE;
+	}
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
 
-   return TRUE;
+	return hWnd;
 }
 
 //
@@ -123,58 +230,61 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Parse the menu selections:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
+	switch (message)
+	{
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		// TODO: Add any drawing code that uses hdc here..
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_DESTROY:
+		isGameRunning = FALSE;
+		PostQuitMessage(0);
+		break;
+	case WM_SIZE:
+		GetClientRect(hWnd, &rct);
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
 }
 
 // Message handler for about box.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
 }
